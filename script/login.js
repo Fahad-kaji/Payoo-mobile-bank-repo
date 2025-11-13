aa7e8958da4807525608748dbb847f5101d87ee5
fahad <fahadkaji.pro@gmail.com>
2025-11-13 23:15:52 +0600

    added add money page

--- script/login.js
+++ script/login.js
@@ -3,6 +3,22 @@ document.getElementById('login-btn')
 .addEventListener('click',function(event){
     event.preventDefault();
     const accountNumber = document.getElementById('account-number').value;
+    const pin = document.getElementById('pin').value;
+    if(accountNumber.length === 11){
+        if(pin.length === 4){
+            window.location.href="./main.html"
+            
+        }
+        else{
+            alert('pin thik nai ')
+            
+        }
+        
+    }
+    else{
+        alert('You need valid Account Number');
+        
+    }
     
     
 })
