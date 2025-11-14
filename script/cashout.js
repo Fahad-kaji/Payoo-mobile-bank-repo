68eb28090676a83ff2f39f4ab1ea5b0cbd386dc3
fahad <fahadkaji.pro@gmail.com>
2025-11-14 19:14:03 +0600

    cash out section adde

---
+++ script/cashout.js
@@ -0,0 +1,19 @@
+document.getElementById('cashout-btn')
+.addEventListener('click',function(event){
+    event.preventDefault();
+
+    const cashoutPin = document.getElementById('cashout-pin').value;
+    const convertedPin = parseInt(cashoutPin);
+    const cashoutAmount = document.getElementById('cashout-amount').value;
+    const convertedAmount = parseFloat(cashoutAmount);
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
+    if(convertedPin === 1234){
+        const sum = convertedMainBalance - convertedAmount;
+        document.getElementById('main-balance').innerText = sum;
+        
+    }
+    else{
+        alert('Enter valid pin')
+    }
+})
